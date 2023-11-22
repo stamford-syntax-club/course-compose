c476a3867929487131ced2d32207c4590fe84818
chinathaip <cartoonabe@gmail.com>
2023-11-22 23:17:17 +0700

    support dynamic directory for finding .env

--- apps/backend/reviews/routers/public_integration_test.go
+++ apps/backend/reviews/routers/public_integration_test.go
@@ -1,20 +1,40 @@
 package routers
 
 import (
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/stamford-syntax-club/course-compose/prisma/db"
+	"github.com/stamford-syntax-club/course-compose/reviews/config"
+	"github.com/stamford-syntax-club/course-compose/reviews/handlers"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetAllReviews(t *testing.T) {
-	app := fiber.New()
-	api := app.Group("/api")
-	registerPublicRoutes(api)
+var (
+	app    *fiber.App
+	testClient *db.PrismaClient
+)
 
-	req := httptest.NewRequest(http.MethodGet, "/api/ITE101/reviews", http.NoBody)
+func setupTest() {
+	config.LoadEnvFile("test")
+	testClient = db.NewClient()
+	if err := testClient.Prisma.Connect(); err != nil {
+		log.Fatalf("Prisma Connect: %v", err)
+	}
+
+	h := handlers.New(testClient)
+
+	app = fiber.New()
+	registerPublicRoutes(app, h)
+}
+
+func TestGetAllReviews(t *testing.T) {
+	setupTest()
+	defer testClient.Prisma.Disconnect()
+	req := httptest.NewRequest(http.MethodGet, "/ITE101/reviews", http.NoBody)
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := app.Test(req, -1)
