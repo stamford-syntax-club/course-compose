4c3d65959a2ca016f065ec8ab4d899f143c44fc4
chinathaip <cartoonabe@gmail.com>
2023-12-02 02:06:52 +0700

    include proj to turbo & add flag for specifying config file

--- apps/api-gateway/main.go
+++ apps/api-gateway/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/stamford-syntax-club/course-compose/api-gateway/config"
 	"github.com/stamford-syntax-club/course-compose/api-gateway/routers"
 )
 
+var configFile string
+
+func init() {
+	flag.StringVar(&configFile, "config", "syntax-config.yaml", "path to config file")
+	flag.Parse()
+}
+
 func main() {
-	gatewayConfig, err := config.ReadGatewayConfig("syntax-config.yaml")
+	gatewayConfig, err := config.ReadGatewayConfig(configFile)
 	if err != nil {
 		log.Fatal("Read gateway config: ", err)
 	}
