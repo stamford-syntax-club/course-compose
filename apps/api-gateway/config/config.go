4133a6642c2076ddae1c0613b0bff61593de60e9
chinathaip <cartoonabe@gmail.com>
2023-12-02 02:39:38 +0700

    dockerize api-gateway + some adjustments

--- apps/api-gateway/config/config.go
+++ apps/api-gateway/config/config.go
@@ -3,7 +3,9 @@ package config
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 
 	"gopkg.in/yaml.v3"
 )
@@ -24,8 +26,36 @@ type Destination struct {
 	IsArray  bool   `yaml:"is_array"`
 }
 
+// findRootDir looks for a marker file (e.g., go.mod) in the current directory
+// and all parent directories until it reaches the root directory.
+// In our case, the root directory is /reviews
+func findRootDir() (string, error) {
+	// Start from the current working directory
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return "", fmt.Errorf("Error getting working directory: %v", err)
+	}
+
+	// Look for a marker file (e.g., go.mod)
+	if _, err := os.Stat(filepath.Join(currentDir, "go.mod")); err == nil {
+		return currentDir, nil
+	}
+
+	// Move up one directory
+	currentDir = filepath.Dir(currentDir)
+
+	return "", fmt.Errorf("Marker file not found, please make sure you are inside the project directory")
+}
+
 func ReadGatewayConfig(filename string) (*Gateway, error) {
-	data, err := os.ReadFile(filename)
+	dir, err := findRootDir()
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("find root dir: %v", err))
+	}
+
+	fullpath := filepath.Join(dir, filename)
+
+	data, err := os.ReadFile(fullpath)
 	if err != nil {
 		return nil, errors.New(fmt.Sprintf("read syntax-config.yaml: %v", err))
 	}
@@ -34,33 +64,34 @@ func ReadGatewayConfig(filename string) (*Gateway, error) {
 	if err := yaml.Unmarshal(data, &gatewayConf); err != nil {
 		return nil, errors.New(fmt.Sprintf("unmarshal syntax-config.yaml: %v", err))
 	}
-    
-    if err := validateGatewayConfig(gatewayConf); err != nil {
-        return nil, err
-    }
 
+	if err := validateGatewayConfig(gatewayConf); err != nil {
+		return nil, err
+	}
+
+	log.Println("loaded config file: ", fullpath)
 	return gatewayConf, nil
 }
 
 func validateGatewayConfig(gatewayConf *Gateway) error {
-    if len(gatewayConf.Routes) <= 0 {
-        return errors.New("no routes found in syntax-config.yaml")
-    }
+	if len(gatewayConf.Routes) <= 0 {
+		return errors.New("no routes found in syntax-config.yaml")
+	}
 
-    for _, route := range gatewayConf.Routes {
-        if route.Path == "" {
-            return errors.New("route path cannot be empty")
-        }
+	for _, route := range gatewayConf.Routes {
+		if route.Path == "" {
+			return errors.New("route path cannot be empty")
+		}
 
-        if route.Method == "" {
-            return errors.New("route method cannot be empty")
-        }
-        
-        if route.Dest.Endpoint == "" {
-            return errors.New("route destination endpoint cannot be empty")
-        }
+		if route.Method == "" {
+			return errors.New("route method cannot be empty")
+		}
 
-        // no need to check for route.Dest.Service because it is only used for logging
-    }
-    return nil
+		if route.Dest.Endpoint == "" {
+			return errors.New("route destination endpoint cannot be empty")
+		}
+
+		// no need to check for route.Dest.Service because it is only used for logging
+	}
+	return nil
 }
