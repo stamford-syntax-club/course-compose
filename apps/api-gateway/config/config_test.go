e055e2e4964db8316b2d847b62bac02aad2d840e
chinathaip <cartoonabe@gmail.com>
2023-12-02 14:03:14 +0700

    refactor: test table for config_test.go

--- apps/api-gateway/config/config_test.go
+++ apps/api-gateway/config/config_test.go
@@ -7,26 +7,42 @@ import (
 )
 
 func TestReadGatewayConfig(t *testing.T) {
-	t.Run("Parse YAML correctly with correct configuration", func(t *testing.T) {
-		_, err := ReadGatewayConfig("testdata/correct.yaml")
-		assert.NoError(t, err)
-	})
+	tests := []struct {
+		name          string
+		configFile    string
+		expectedError string
+	}{
+		{
+			name:       "Parse YAML correctly with correct configuration",
+			configFile: "testdata/correct.yaml",
+		},
+		{
+			name:          "Return error when no routes are found",
+			configFile:    "testdata/no-routes.yaml",
+			expectedError: "no routes found in syntax-config.yaml",
+		},
+		{
+			name:          "Return error when route path is empty",
+			configFile:    "testdata/route-path-empty.yaml",
+			expectedError: "route path cannot be empty",
+		},
+		{
+			name:          "Return error when route method is empty",
+			configFile:    "testdata/route-method-empty.yaml",
+			expectedError: "route method cannot be empty",
+		},
+	}
 
-	t.Run("Return error when no routes are found", func(t *testing.T) {
-		_, err := ReadGatewayConfig("testdata/no-routes.yaml")
-		assert.Error(t, err)
-		assert.Equal(t, "no routes found in syntax-config.yaml", err.Error())
-	})
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := ReadGatewayConfig(test.configFile)
 
-	t.Run("Return error when route path is empty", func(t *testing.T) {
-		_, err := ReadGatewayConfig("testdata/route-path-empty.yaml")
-		assert.Error(t, err)
-		assert.Equal(t, "route path cannot be empty", err.Error())
-	})
-
-	t.Run("Return error when route method is empty", func(t *testing.T) {
-		_, err := ReadGatewayConfig("testdata/route-method-empty.yaml")
-		assert.Error(t, err)
-		assert.Equal(t, "route method cannot be empty", err.Error())
-	})
+			if test.expectedError != "" {
+				assert.Error(t, err)
+				assert.Equal(t, test.expectedError, err.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
