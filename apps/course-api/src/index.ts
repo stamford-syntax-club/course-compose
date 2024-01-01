816242e8f4367fe9066781a3c980592fe8c7b7a3
chinathaip <cartoonabe@gmail.com>
2024-01-02 00:46:39 +0700

    feat: initial get all courses + pagination

---
+++ apps/course-api/src/index.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+
+const port = process.env.PORT || 8002;
+
+app.listen(port, () => {
+	console.log(`Server is 🔥 at http://localhost:${port}`);
+});
