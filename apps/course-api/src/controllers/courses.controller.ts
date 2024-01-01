a5d78369996bdd6841e018c4591737ae529ff122
chinathaip <cartoonabe@gmail.com>
2024-01-02 01:39:08 +0700

    feat: search course by code or full name

--- apps/course-api/src/controllers/courses.controller.ts
+++ apps/course-api/src/controllers/courses.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import prismaClient from "../utils/prisma_utils";
 import { paginate } from "../utils/pagination";
 
-// TODO: Add search api/courses?search=..
+// TODO: some caching here
 const getAllCourses = async (req: Request, res: Response) => {
 	const pageSize = parseInt(req.query.pageSize as string) ? parseInt(req.query.pageSize as string) : 10;
 	const pageNumber = parseInt(req.query.pageNumber as string) ? parseInt(req.query.pageNumber as string) : 1;
@@ -13,18 +13,38 @@ const getAllCourses = async (req: Request, res: Response) => {
 		});
 	}
 
+	const searchQuery = {
+		where: {
+			OR: [
+				{
+					code: {
+						contains: (req.query.search as string) || ""
+					}
+				},
+				{
+					full_name: {
+						contains: (req.query.search as string) || ""
+					}
+				}
+			]
+		}
+	};
+
 	try {
 		const courses = await prismaClient.course.findMany({
+			...searchQuery,
 			skip: pageSize * (pageNumber - 1),
 			take: pageSize
 		});
 
-		const courseCount = await prismaClient.course.count();
+		const courseCount = await prismaClient.course.count({
+			...searchQuery
+		});
 
-        const data = paginate(courses, pageSize, pageNumber, courseCount);
+		const data = paginate(courses, pageSize, pageNumber, courseCount);
 
-		res.status(200).json(data);	
-    } catch (error) {
+		res.status(200).json(data);
+	} catch (error) {
 		console.log("Error: ", error);
 	}
 };
