cce0683f9aa1e07641259679ee9d42cd82f7eb5c
chinathaip <cartoonabe@gmail.com>
2024-01-02 01:27:58 +0700

    add more fields to pagination

--- apps/course-api/src/controllers/courses.controller.ts
+++ apps/course-api/src/controllers/courses.controller.ts
@@ -1,18 +1,30 @@
 import { Request, Response } from "express";
 import prismaClient from "../utils/prisma_utils";
+import { paginate } from "../utils/pagination";
 
 // TODO: Add search api/courses?search=..
 const getAllCourses = async (req: Request, res: Response) => {
 	const pageSize = parseInt(req.query.pageSize as string) ? parseInt(req.query.pageSize as string) : 10;
 	const pageNumber = parseInt(req.query.pageNumber as string) ? parseInt(req.query.pageNumber as string) : 1;
 
+	if (pageSize < 1 || pageNumber < 1) {
+		return res.status(400).json({
+			message: "Invalid page size or page number"
+		});
+	}
+
 	try {
 		const courses = await prismaClient.course.findMany({
 			skip: pageSize * (pageNumber - 1),
 			take: pageSize
 		});
-		return res.status(200).json(courses);
-	} catch (error) {
+
+		const courseCount = await prismaClient.course.count();
+
+        const data = paginate(courses, pageSize, pageNumber, courseCount);
+
+		res.status(200).json(data);	
+    } catch (error) {
 		console.log("Error: ", error);
 	}
 };
---
+++ apps/course-api/src/utils/pagination.ts
@@ -0,0 +1,14 @@
+import { Pagination } from "../models/pagination.model";
+
+export function paginate(data: any, pageSize: number, pageNumber: number, totalNumberOfItems: number): Pagination {
+	const totalPages = Math.floor((totalNumberOfItems + pageSize - 1) / pageSize);
+	return {
+		pageInformation: {
+			size: pageSize,
+			number: pageNumber
+		},
+		totalNumberOfItems: totalNumberOfItems,
+		totalPages: totalPages,
+		data: data
+	};
+}
