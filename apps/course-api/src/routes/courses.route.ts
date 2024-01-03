3eaf37e349505fd3a6befd0a51243d1698adc59d
chinathaip <cartoonabe@gmail.com>
2024-01-03 21:28:50 +0700

    feat: get course by code

--- apps/course-api/src/routes/courses.route.ts
+++ apps/course-api/src/routes/courses.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { handleGetAllCourses } from "../controllers/courses.controller";
+import { handleGetAllCourses, handleGetCourseByCode } from "../controllers/courses.controller";
 
 const courseRouter = express.Router();
 
 courseRouter.get("/", handleGetAllCourses);
+courseRouter.get("/:code", handleGetCourseByCode);
 
 export default courseRouter;
