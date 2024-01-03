d7267210cdf06189a0a716703a14abd07479fe2a
chinathaip <cartoonabe@gmail.com>
2024-01-03 21:29:00 +0700

    add logging

--- apps/course-api/src/app.ts
+++ apps/course-api/src/app.ts
@@ -2,10 +2,12 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import prismaClient from "./utils/prisma_utils";
 import courseRouter from "./routes/courses.route";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(cors());
+app.use(morgan("combined"));
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Welcome to Express & TypeScript Server");
