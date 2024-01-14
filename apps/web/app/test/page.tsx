98a477105218ffa909364ab3f6609df50ba17966
Lxkas <alohasunshineday@yahoo.com>
2024-01-15 00:16:03 +0700

    frontend skeleton draft

---
+++ apps/web/app/test/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Paper } from "@mantine/core";
+
+export default function TestPage(): JSX.Element {
+	return (
+		<Paper bg="dark.8" withBorder className="h-full w-64">
+			Lol
+		</Paper>
+	);
+}
---
+++ apps/web/components/ui/course-card.tsx
@@ -0,0 +1,70 @@
+import { Badge, Button, Card, Grid, Group, Rating, Stack, Text } from "@mantine/core";
+
+interface CourseCardProps {
+	courseName: string;
+	courseCode: string;
+	coursePrerequisites?: string[];
+	courseRating: number;
+	courseReviewCount: number;
+}
+
+export function CourseCard({
+	courseName,
+	courseCode,
+	coursePrerequisites = [],
+	courseRating,
+	courseReviewCount
+}: CourseCardProps): JSX.Element {
+	const coursePreReqString =
+		coursePrerequisites.length > 0 ? `Pre: ${coursePrerequisites.join(", ")}` : "No Prerequisites";
+
+	return (
+		<Grid.Col span={{ base: 12, sm: 6, xl: 4 }}>
+			<Card
+				className="h-52 shadow-sm transition-shadow duration-150 hover:shadow-lg"
+				padding="lg"
+				radius="md"
+				withBorder
+			>
+				<Stack className="h-full" gap="xs">
+					<Group wrap="nowrap">
+						<Badge className="min-w-fit" color="blue">
+							IT
+						</Badge>
+
+						<Text title={courseName} component="div" truncate="end">
+							{courseName}
+						</Text>
+
+						<Text
+							fz={{
+								base: "sm",
+								sm: "md"
+							}}
+						>
+							<Text span c="dimmed" inherit>
+								({courseCode})
+							</Text>
+						</Text>
+					</Group>
+
+					<Text truncate="end" title={coursePreReqString} c="dimmed">
+						{coursePreReqString}
+					</Text>
+
+					<Group gap="xs">
+						<Rating value={courseRating} fractions={2} readOnly />
+						<Text>
+							{courseRating} ({courseReviewCount})
+						</Text>
+					</Group>
+
+					<Group className="mt-auto" justify="flex-end" gap="xs">
+						<Button variant="outline">View</Button>
+						<Button>Review</Button>
+					</Group>
+				</Stack>
+			</Card>
+		</Grid.Col>
+	);
+}
