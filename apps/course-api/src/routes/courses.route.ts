a315dc446128cbe90960319f90076ad78d027da8
chinathaip <cartoonabe@gmail.com>
2024-01-14 23:56:05 +0700

    admin auth for adding new courses

--- apps/course-api/src/routes/courses.route.ts
+++ apps/course-api/src/routes/courses.route.ts
@@ -1,12 +1,11 @@
 import express from "express";
 import { handleAddCourses, handleGetAllCourses, handleGetCourseByCode } from "@controllers/courses.controller";
+import adminAuth from "@middlewares/admin";
 
 const courseRouter = express.Router();
 
 courseRouter.get("/", handleGetAllCourses);
 courseRouter.get("/:code", handleGetCourseByCode);
-
-// TODO: should be protected with a basic auth
-courseRouter.post("/", handleAddCourses);
+courseRouter.post("/", adminAuth(), handleAddCourses);
 
 export default courseRouter;
