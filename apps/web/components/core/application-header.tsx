fb9c2e1f844e37519cf28a21bbecca63c7c76c9c
Lxkas <alohasunshineday@yahoo.com>
2024-01-15 18:04:10 +0700

    remove theme switcher 'cuz more problems than it's worth

--- apps/web/components/core/application-header.tsx
+++ apps/web/components/core/application-header.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import { AppShell, Burger, Group, Switch, useComputedColorScheme, useMantineColorScheme } from "@mantine/core";
-import { IconMoon, IconSun } from "@tabler/icons-react";
+import { AppShell, Burger, Group } from "@mantine/core";
 import Link from "next/link";
 
 export default function ApplicationHeader({ opened, toggle }: { opened: boolean; toggle: () => void }): JSX.Element {
-	const { setColorScheme } = useMantineColorScheme({
-		keepTransitions: true
-	});
-	const colorScheme = useComputedColorScheme("dark");
-
 	return (
 		<AppShell.Header>
 			<Group h="100%" px="md">
@@ -18,19 +12,6 @@ export default function ApplicationHeader({ opened, toggle }: { opened: boolean;
 					<Link href="/">
 						<span className="select-none text-3xl font-bold uppercase">Course Compose</span>
 					</Link>
-				</div>
-
-				<div className="ml-auto">
-					<Switch
-						onChange={(_) => {
-							setColorScheme(colorScheme === "dark" ? "light" : "dark");
-						}}
-						checked={colorScheme === "dark"}
-						size="md"
-						color="dark.4"
-						offLabel={<IconSun className="size-4" />}
-						onLabel={<IconMoon className="size-4" />}
-					/>
 				</div>
 			</Group>
 		</AppShell.Header>
