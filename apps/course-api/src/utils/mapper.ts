c6377b773b602184990c11d9d6ab0d46d7f6b343
chinathaip <cartoonabe@gmail.com>
2024-01-16 12:20:53 +0700

    unit test for calcOverallRating

--- apps/course-api/src/utils/mapper.ts
+++ apps/course-api/src/utils/mapper.ts
@@ -1,12 +1,12 @@
 import { CourseResponse, RawCourse } from "@models/course.response.model";
 
-function calcOverallRating(reviews: { rating: number }[]): number {
+export function calcOverallRating(reviews: { rating: number }[]): number {
 	let sum = 0;
 	for (let i = 0; i < reviews.length; i++) {
 		sum += reviews[i].rating;
 	}
 
-	return sum / reviews.length;
+	return sum / reviews.length || 0;
 }
 
 export function mapCourseToCourseResponse(course: RawCourse): CourseResponse {
