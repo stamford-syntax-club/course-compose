10afa526808d7df848d927f405db1c5302cf9171
Oak <minkojee@gmail.com>
2024-02-17 23:23:42 +0700

    created UI for my-review-card

--- apps/web/components/ui/cards/my-review-card.tsx
+++ apps/web/components/ui/cards/my-review-card.tsx
@@ -1,4 +1,5 @@
-import { Button, Card, Group, Rating, Stack, Text } from "@mantine/core";
+import { Button, Card, Flex, Group, Image, Rating, Stack, Text } from "@mantine/core";
+import { limitWords } from "@utils/limit-text";
 
 interface CourseCardProps {
 	courseName: string;
@@ -22,34 +23,46 @@ export function MyReviewCard({
 		// base: 12, sm: 6, xl: 4
 		<div className="col-span-12 sm:col-span-6 xl:col-span-4">
 			<Card
-				bg="blue"
+				bg="dark.7"
 				className="bg-transparent shadow-sm transition-colors duration-150 hover:border-gray-400"
 				padding="md"
 				radius="md"
 				withBorder
 			>
 				<Stack className="h-full" gap="xs">
-					<Stack gap="0">
-						<Text span c="dimmed" inherit>
-							{courseCode}
-						</Text>
-						<Text title={courseName} component="div" truncate="end">
-							{courseName}
-						</Text>
-						<Text truncate="end" title={coursePreReqString} c="dimmed">
-							{coursePreReqString}
-						</Text>
-					</Stack>
+					<Group style={{ padding: "10px", borderRadius: "8px" }} bg="dark.6">
+						<Stack gap="0">
+							<Text span c="dimmed" inherit>
+								{courseCode}
+							</Text>
+							<Text title={courseName} component="div" truncate="end">
+								{courseName}
+							</Text>
+							<Text truncate="end" title={coursePreReqString} c="dimmed">
+								{coursePreReqString}
+							</Text>
+						</Stack>
+					</Group>
 
-					<Group gap="xs">
+					<Flex gap="xs" align="center" justify="space-between">
 						<Rating value={courseRating} fractions={2} readOnly />
-						{/* <Text>
-							{courseRating} ({courseReviewCount})
-						</Text> */}
+						<Flex gap="5px" align="center">
+							<Image alt="uparrowgreen" height={6} src="uparrowgreen.svg" width={4} />
+							30
+							<Image alt="downarrowred" height={6} src="downarrowred.svg" width={4} />
+							40
+						</Flex>
+					</Flex>
+
+					<Group>
+						{limitWords(
+							"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis ultricies lacus sed.",
+							30
+						)}
 					</Group>
 
 					<Group className="mt-auto w-full" justify="flex-end" gap="xs">
-						<Button>GO TO MY REVIEW</Button>
+						<Button bg="blue.4">GO TO MY REVIEW</Button>
 					</Group>
 				</Stack>
 			</Card>
