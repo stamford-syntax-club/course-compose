3b02109d3d4237f322170756746c1172579f0bd1
Oak <minkojee@gmail.com>
2024-03-02 22:15:21 +0700

    fixed feedbacks

--- apps/web/utils/limit-text.ts
+++
@@ -1,7 +0,0 @@
-export const limitWords = (text: string, limit: number) => {
-	const words = text.split(/\s+/);
-	if (words.length > limit) {
-		return `${words.slice(0, limit).join(" ")}...`;
-	}
-	return text;
-};
