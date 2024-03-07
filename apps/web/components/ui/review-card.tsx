c558f6a67d7f1d4f43549d59e8ef0e5bdaf707e1
chinathaip <cartoonabe@gmail.com>
2024-03-07 23:23:28 +0700

    format

--- apps/web/components/ui/review-card.tsx
+++ apps/web/components/ui/review-card.tsx
@@ -1,4 +1,15 @@
-import { Avatar, Badge, Blockquote, Card, Flex, Menu, Rating, Spoiler, Text, TypographyStylesProvider } from "@mantine/core";
+import {
+	Avatar,
+	Badge,
+	Blockquote,
+	Card,
+	Flex,
+	Menu,
+	Rating,
+	Spoiler,
+	Text,
+	TypographyStylesProvider
+} from "@mantine/core";
 import { IconEdit, IconDots, IconX } from "@tabler/icons-react";
 import ReactMarkdown from "react-markdown";
 import { Review } from "types/reviews";
@@ -60,19 +71,31 @@ export function MyReviewCard({ review }: ReviewCardProps) {
 		<Card bg="gray.9" padding="md" radius="md" withBorder className={`border-${getStatusColor(review.status)}-500`}>
 			<Flex direction="row" gap="md" justify="center">
 				{/* user profile and badges */}
-				<Flex direction="column" align="center" gap="4" justify="center" className="max-w-sm text-center" visibleFrom="md">
+				<Flex
+					direction="column"
+					align="center"
+					gap="4"
+					justify="center"
+					className="max-w-sm text-center"
+					visibleFrom="md"
+				>
 					<Avatar size="70" src={null} alt="anonymous profile" />
 					<Text>You</Text>
 					<Badge color={getStatusColor(review.status)}>{review.status}</Badge>
 				</Flex>
 
 				{/* review and ratings */}
-				
-				<Flex direction="column" justify="flex-start" ml="3" gap="xs" w="100%" >
-				<Badge color={getStatusColor(review.status)} hiddenFrom="md">{review.status}</Badge>
-				{review.status === "REJECTED" && <Blockquote color="red" w="100%" p="sm" mb="xs"><Text>Your review has been rejected Your review has been rejected</Text></Blockquote>}
-			
-					
+
+				<Flex direction="column" justify="flex-start" ml="3" gap="xs" w="100%">
+					<Badge color={getStatusColor(review.status)} hiddenFrom="md">
+						{review.status}
+					</Badge>
+					{review.status === "REJECTED" && (
+						<Blockquote color="red" w="100%" p="sm" mb="xs">
+							<Text>Your review has been rejected Your review has been rejected</Text>
+						</Blockquote>
+					)}
+
 					<Rating size="md" value={review.rating} fractions={2} defaultValue={0} readOnly />
 					<Text fw={800} size="md">
 						Academic Year: {review.academicYear}
