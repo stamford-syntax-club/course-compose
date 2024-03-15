a44549e37ba7f6cc98070820274d156cd165a422
chinathaip <cartoonabe@gmail.com>
2024-03-15 15:42:42 +0700

    course compose api client

--- apps/web/app/courses/[courseCode]/page.tsx
+++ apps/web/app/courses/[courseCode]/page.tsx
@@ -9,20 +9,13 @@ import { Review } from "types/reviews";
 import BackButton from "@components/ui/back-button";
 import { useDisclosure } from "@mantine/hooks";
 import Link from "next/link";
-import { ErrorResponse } from "types/errors";
 import { notifications } from "@mantine/notifications";
 import WriteReviewForm from "@components/ui/write-review-form";
 import SessionModal from "@components/ui/session-modal";
-import {
-	ERR_EXPIRED_TOKEN,
-	ERR_REVIEW_EXIST,
-	ERR_MISSING_TOKEN,
-	ERR_USER_NOT_EXIST,
-	BASE_API_ENDPOINT
-} from "@utils/constants";
+import { BASE_API_ENDPOINT } from "@utils/constants";
 import { Session } from "@supabase/supabase-js";
 import { useSupabaseStore } from "@stores/supabase-store";
-import { fetchCourseDetails, fetchCourseReviews } from "lib/api/api";
+import CourseComposeAPIClient from "lib/api/api";
 
 export default function CourseReview({ params }: { params: { courseCode: string } }) {
 	const [courseData, setCourseData] = useState<Course>();
@@ -36,52 +29,7 @@ export default function CourseReview({ params }: { params: { courseCode: string 
 	const COURSE_ENDPOINT = `${BASE_API_ENDPOINT}/courses/${params.courseCode}`;
 	const REVIEW_ENDPOINT = `${COURSE_ENDPOINT}/reviews`;
 
-	const submitReview = async (academicYear: string, description: string, rating: number) => {
-		const res = await fetch(REVIEW_ENDPOINT, {
-			method: "POST",
-			body: JSON.stringify({
-				academic_year: parseInt(academicYear),
-				description: description,
-				rating: rating
-			}),
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${sessionData?.access_token ? sessionData?.access_token : ""}`
-			}
-		});
-		const data = await res.json();
-
-		switch ((data as ErrorResponse).message) {
-			case undefined:
-				notifications.show({
-					title: "Submit review successfully",
-					message: "Your review has been submitted. Thank you so much for making Stamford a better place!",
-					color: "green"
-				});
-				setTimeout(() => {
-					window.location.reload();
-				}, 5000);
-				break;
-			case ERR_REVIEW_EXIST:
-				notifications.show({
-					title: ERR_REVIEW_EXIST,
-					message: "You can either edit or delete your existing review",
-					color: "red",
-					autoClose: 5000
-				});
-				break;
-			case ERR_MISSING_TOKEN || ERR_EXPIRED_TOKEN || ERR_USER_NOT_EXIST:
-				open(); // session modal
-				break;
-			default:
-				notifications.show({
-					title: "Something is wrong on our end",
-					message: "Your review cannot be submitted yet, please try again later",
-					color: "red",
-					autoClose: 5000
-				});
-		}
-	};
+	const apiClient = new CourseComposeAPIClient(params.courseCode);
 
 	const editReview = async (id: number, academicYear: string, description: string, rating: number) => {
 		const res = await fetch(`${REVIEW_ENDPOINT}/edit`, {
@@ -103,23 +51,8 @@ export default function CourseReview({ params }: { params: { courseCode: string 
 	};
 
 	useEffect(() => {
-		if (isLoading) {
-			return; // prevent fetching before the session is retrieved
-		}
-
-		setIsLoading(true);
-
-		fetchCourseReviews(params.courseCode, pageNumber, sessionData?.access_token || "")
-			.then((reviews) => setReviewsData(reviews))
-			.catch(() => open()) // expired token
-			.finally(() => setIsLoading(false));
-
-		if (!sessionData) open();
-		else close();
-	}, [pageNumber, sessionData]);
-
-	useEffect(() => {
-		fetchCourseDetails(params.courseCode)
+		apiClient
+			.fetchCourseDetails()
 			.then((course) => setCourseData(course))
 			.catch((error) => console.error(error)); // TODO: handle if no course found? - maybe redirect to home page
 
@@ -137,6 +70,23 @@ export default function CourseReview({ params }: { params: { courseCode: string 
 				setIsLoading(false);
 			});
 	}, [supabase]);
+
+	useEffect(() => {
+		if (isLoading) {
+			return; // prevent fetching before the session is retrieved
+		}
+
+		setIsLoading(true);
+
+		apiClient
+			.fetchCourseReviews(pageNumber, sessionData?.access_token || "")
+			.then((reviews) => setReviewsData(reviews))
+			.catch(() => open()) // expired token
+			.finally(() => setIsLoading(false));
+
+		if (!sessionData) open();
+		else close();
+	}, [pageNumber, sessionData]);
 
 	return (
 		<Container>
@@ -212,7 +162,14 @@ export default function CourseReview({ params }: { params: { courseCode: string 
 				Write a Review
 			</Title>
 			<WriteReviewForm
-				onSubmit={(academicYear, description, rating) => submitReview(academicYear, description, rating)}
+				onSubmit={(academicYear, description, rating) =>
+					apiClient
+						.submitReview(academicYear, description, rating, sessionData?.access_token || "")
+						.then((result) => {
+							if (result.title && result.message) notifications.show(result);
+							else open();
+						})
+				}
 			/>
 		</Container>
 	);
--- apps/web/lib/api/api.ts
+++ apps/web/lib/api/api.ts
@@ -2,97 +2,94 @@ import { Course } from "types/course";
 import { Review } from "types/reviews";
 import { ErrorResponse } from "types/errors";
 import { PaginatedResponse } from "types/pagination";
+import {
+	ERR_EXPIRED_TOKEN,
+	ERR_REVIEW_EXIST,
+	ERR_USER_NOT_EXIST,
+	ERR_MISSING_TOKEN,
+	BASE_API_ENDPOINT
+} from "@utils/constants";
+import { NotificationData } from "@mantine/notifications";
 
-const HOST_NAME: string = (() => {
-	switch (process.env.APP_ENV) {
-		case "production" || "beta":
-			return "api-gateway"; // this is resolvable within docker bridge network
-		default:
-			console.warn(`Unexpected APP_ENV value: ${process.env.APP_ENV}. Falling back to localhost.`);
-			return "localhost"; // otherwise we send traffic to local api gateway
-	}
-})();
+export default class CourseComposeAPIClient {
+	private courseEndpoint: string;
+	private reviewEndpoint: string;
 
-const BASE_API_ENDPOINT = `http://${HOST_NAME}:8000/api`;
-const COURSE_ENDPOINT = `${BASE_API_ENDPOINT}/courses`;
-
-function createReviewEndpoint(courseCode: string): string {
-	return `${COURSE_ENDPOINT}/${courseCode}/reviews`;
-}
-
-export async function fetchCourseDetails(courseCode: string): Promise<Course> {
-	const data = await fetch(`${COURSE_ENDPOINT}/${courseCode}`);
-	return data.json() as Promise<Course>;
-}
-
-export async function fetchCourseReviews(
-	courseCode: string,
-	pageNumber: number,
-	accessToken: string
-): Promise<PaginatedResponse<Review>> {
-	const data = await fetch(`${createReviewEndpoint(courseCode)}?pageNumber=${pageNumber}&pageSize=10`, {
-		headers: {
-			Authorization: `Bearer ${accessToken}`
-		}
-	});
-
-	if (!data.ok) {
-		const err = (await data.json()) as ErrorResponse;
-		throw new Error(err.message);
+	constructor(courseCode: string) {
+		this.courseEndpoint = `${BASE_API_ENDPOINT}/courses/${courseCode}`;
+		this.reviewEndpoint = `${this.courseEndpoint}/reviews`;
 	}
 
-	return data.json() as Promise<PaginatedResponse<Review>>;
-}
+	async fetchCourseDetails(): Promise<Course> {
+		const data = await fetch(this.courseEndpoint);
+		return data.json() as Promise<Course>;
+	}
 
-export async function submitReview(
-	courseCode: string,
-	academicYear: string,
-	description: string,
-	rating: number,
-	accessToken: string
-) {
-	const res = await fetch(createReviewEndpoint(courseCode), {
-		method: "POST",
-		body: JSON.stringify({
-			academic_year: parseInt(academicYear),
-			description: description,
-			rating: rating
-		}),
-		headers: {
-			"Content-Type": "application/json",
-			Authorization: `Bearer ${accessToken}`
+	async fetchCourseReviews(pageNumber: number, accessToken: string): Promise<PaginatedResponse<Review>> {
+		const data = await fetch(`${this.reviewEndpoint}?pageNumber=${pageNumber}&pageSize=10`, {
+			headers: {
+				Authorization: `Bearer ${accessToken}`
+			}
+		});
+
+		if (!data.ok) {
+			const err = (await data.json()) as ErrorResponse;
+			throw new Error(err.message);
 		}
-	});
-	const data = await res.json();
 
-	switch ((data as ErrorResponse).message) {
-		case undefined:
-			notifications.show({
+		return data.json() as Promise<PaginatedResponse<Review>>;
+	}
+
+	async submitReview(
+		academicYear: string,
+		description: string,
+		rating: number,
+		accessToken: string
+	): Promise<NotificationData> {
+		const data = await fetch(this.reviewEndpoint, {
+			method: "POST",
+			body: JSON.stringify({
+				academic_year: parseInt(academicYear),
+				description: description,
+				rating: rating
+			}),
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${accessToken}`
+			}
+		});
+
+		if (data.ok) {
+			return {
 				title: "Submit review successfully",
 				message: "Your review has been submitted. Thank you so much for making Stamford a better place!",
 				color: "green"
-			});
-			setTimeout(() => {
-				window.location.reload();
-			}, 5000);
-			break;
-		case ERR_REVIEW_EXIST:
-			notifications.show({
-				title: ERR_REVIEW_EXIST,
-				message: "You can either edit or delete your existing review",
-				color: "red",
-				autoClose: 5000
-			});
-			break;
-		case ERR_MISSING_TOKEN || ERR_EXPIRED_TOKEN || ERR_USER_NOT_EXIST:
-			open(); // session modal
-			break;
-		default:
-			notifications.show({
-				title: "Something is wrong on our end",
-				message: "Your review cannot be submitted yet, please try again later",
-				color: "red",
-				autoClose: 5000
-			});
+			};
+		}
+
+		const errMsg = ((await data.json()) as ErrorResponse).message;
+		switch (errMsg) {
+			case ERR_REVIEW_EXIST:
+				return {
+					title: errMsg,
+					message: "You can either edit or delete your existing review",
+					color: "red",
+					autoClose: 5000
+				};
+			case ERR_MISSING_TOKEN:
+			case ERR_EXPIRED_TOKEN:
+			case ERR_USER_NOT_EXIST:
+				return {
+					title: "",
+					message: ""
+				};
+			default:
+				return {
+					title: "Something is wrong on our end",
+					message: "Your review cannot be submitted yet, please try again later",
+					color: "red",
+					autoClose: 5000
+				};
+		}
 	}
 }
