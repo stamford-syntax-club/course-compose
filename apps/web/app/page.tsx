1149ce86e4318f51835979e1568ffbd2d624bfaa
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-04-05 00:15:26 +0700

    ana/home-page-update

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,28 @@
 "use client";
 
-import { CourseCard } from "@components/ui/cards/course-card";
-import { Button, Grid, Group, Menu, Pagination, Paper, Stack, Text, TextInput, rem } from "@mantine/core";
+import { CourseCard } from "@components/ui/course-card";
 import {
-	IconArrowsLeftRight,
+	Button,
+	Grid,
+	Group,
+	Menu,
+	Pagination,
+	Paper,
+	Stack,
+	Text,
+	TextInput,
+	Container,
+	rem,
+	Slider
+} from "@mantine/core";
+import { IconSortAscending, IconSortAscendingNumbers, IconSortDescendingNumbers } from "@tabler/icons-react";
+import {
+	IconFileDescription,
 	IconFilter,
-	IconMessageCircle,
-	IconPhoto,
+	IconReload,
 	IconSearch,
-	IconSettings,
-	IconTrash
+	IconSortDescending,
+	IconStarFilled
 } from "@tabler/icons-react";
 import { useState } from "react";
 
@@ -67,136 +80,146 @@ const COURSE_LIST = [
 
 export default function HomePage(): JSX.Element {
 	const [currentSearch, setCurrentSearch] = useState("");
+	const [value, setValue] = useState(0);
+	const [endValue, setEndValue] = useState(200);
 
 	const filteredCourses = currentSearch
 		? COURSE_LIST.filter((course) => {
-				return course.full_name.toLowerCase().includes(currentSearch.toLowerCase());
+				const COURSE_NAME_MATCH = course.courseName.toLowerCase().includes(currentSearch.toLowerCase());
+				const COURSE_CODE_MATCH = course.courseCode.toLowerCase().includes(currentSearch.toLowerCase());
+				const COURSE_PREREQUISITES_MATCH = course.coursePrerequisites.some((prerequisite) =>
+					prerequisite.toLowerCase().includes(currentSearch.toLowerCase())
+				);
+
+				return COURSE_NAME_MATCH || COURSE_CODE_MATCH || COURSE_PREREQUISITES_MATCH;
+				// return course.courseName.toLowerCase().includes(currentSearch.toLowerCase());
 			})
 		: COURSE_LIST;
 
 	return (
-		<Grid
-			className="h-full"
-			classNames={{
-				inner: "h-full"
-			}}
-		>
-			<Grid.Col span={{ base: 12, xl: 12 }}>
-				<Stack className="h-full" gap="md">
-					{/* Searchbar Container */}
-					<form
-						onSubmit={(e) => {
-							e.preventDefault();
-							const formData = new FormData(e.target as HTMLFormElement);
-							setCurrentSearch(formData.get("course-searchbar") as string);
-						}}
-					>
-						<Group wrap="nowrap" gap="xs">
-							<TextInput
-								name="course-searchbar"
-								leftSection={<IconSearch className="size-4" />}
-								className="w-full"
-								placeholder="Search"
-								onChange={(e) => {
-									if (e.target.value === "") {
-										setCurrentSearch("");
-									}
-								}}
-							/>
+		<Container fluid>
+			{/*	<Grid.Col span={{ base: 12, xl: 10 }}> */}
+			<Stack className="h-full" gap="md">
+				{/* Searchbar Container */}
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						const formData = new FormData(e.target as HTMLFormElement);
+						setCurrentSearch(formData.get("course-searchbar") as string);
+					}}
+				>
+					<Group wrap="nowrap" gap="xs">
+						<TextInput
+							name="course-searchbar"
+							leftSection={<IconSearch className="size-4" />}
+							className="w-full"
+							placeholder="Search"
+							onChange={(e) => {
+								if (e.target.value === "") {
+									setCurrentSearch("");
+								}
+							}}
+						/>
 
-							{/* Dropdown Menu for Filter */}
-							<Menu shadow="md" width={200}>
-								<Menu.Target>
-									<Button type="button" variant="outline">
-										<IconFilter className="size-4" aria-label="Filter" />
-									</Button>
-								</Menu.Target>
+						{/* Dropdown Menu for Filter */}
+						<Menu shadow="md" width={200}>
+							<Menu.Target>
+								<Button type="button" variant="outline">
+									<IconFilter className="size-4" aria-label="Filter" />
+								</Button>
+							</Menu.Target>
 
-								<Menu.Dropdown>
-									<Menu.Label>Filters</Menu.Label>
-									<Menu.Item
-										leftSection={<IconSettings style={{ width: rem(14), height: rem(14) }} />}
-									>
-										Settings
-									</Menu.Item>
-									<Menu.Item
-										leftSection={<IconMessageCircle style={{ width: rem(14), height: rem(14) }} />}
-									>
-										Messages
-									</Menu.Item>
-									<Menu.Item leftSection={<IconPhoto style={{ width: rem(14), height: rem(14) }} />}>
-										Gallery
-									</Menu.Item>
-									<Menu.Item
-										leftSection={<IconSearch style={{ width: rem(14), height: rem(14) }} />}
-										rightSection={
-											<Text size="xs" c="dimmed">
-												⌘K
-											</Text>
-										}
-									>
-										Search
-									</Menu.Item>
+							<Menu.Dropdown>
+								<Menu.Label>Filters by ratings</Menu.Label>
+								<Menu.Item leftSection={<IconStarFilled style={{ width: rem(14), height: rem(14) }} />}>
+									<Text className="flex items-center justify-between">
+										<span>High to Low</span>
+										<IconSortDescending style={{ width: rem(18), height: rem(18) }} />
+									</Text>
+								</Menu.Item>
+								<Menu.Item leftSection={<IconStarFilled style={{ width: rem(14), height: rem(14) }} />}>
+									<Text className="flex items-center justify-between">
+										<span>Low to High</span>
+										<IconSortAscending style={{ width: rem(18), height: rem(18) }} />
+									</Text>
+								</Menu.Item>
 
-									<Menu.Divider />
+								<Menu.Divider />
 
-									<Menu.Label>Danger zone</Menu.Label>
-									<Menu.Item
-										leftSection={
-											<IconArrowsLeftRight style={{ width: rem(14), height: rem(14) }} />
-										}
-									>
-										Transfer my data
-									</Menu.Item>
-									<Menu.Item
-										color="red"
-										leftSection={<IconTrash style={{ width: rem(14), height: rem(14) }} />}
-									>
-										Delete my account
-									</Menu.Item>
-								</Menu.Dropdown>
-							</Menu>
+								<Menu.Label>Filter by review count</Menu.Label>
+								<Menu.Item
+									leftSection={<IconFileDescription style={{ width: rem(14), height: rem(14) }} />}
+								>
+									<Text className="flex items-center justify-between">
+										<span>High to Low</span>
+										<IconSortDescendingNumbers style={{ width: rem(18), height: rem(18) }} />
+									</Text>
+								</Menu.Item>
+								<Menu.Item
+									leftSection={<IconFileDescription style={{ width: rem(14), height: rem(14) }} />}
+								>
+									<Text className="flex items-center justify-between">
+										<span>Low to High</span>
+										<IconSortAscendingNumbers style={{ width: rem(18), height: rem(18) }} />
+									</Text>
+								</Menu.Item>
 
-							{/* Search Button */}
-							<Button type="submit">
-								<IconSearch className="size-4" aria-label="Filter" />
-							</Button>
-						</Group>
-					</form>
+								<Menu.Divider />
+								<Menu.Item>
+									<Text mt="md" size="sm">
+										Review count <b>{value}</b>
+									</Text>
+									<Slider value={value} onChange={setValue} onChangeEnd={setEndValue} />
+								</Menu.Item>
 
-					{/* Courses List Body */}
-					<Paper bg="dark.8" p="sm" withBorder className="h-full">
-						<div className="relative flex size-full flex-col">
-							<div className="grid grid-cols-12 grid-rows-3 gap-4">
-								{filteredCourses.map((course) => {
-									return (
-										<CourseCard
-											// Ensure that the key is unique, otherwise same keys will cause a lot of issues.
-											key={`CourseCard_${course.code}`}
-											full_name={course.full_name}
-											code={course.code}
-											prerequisites={course.prerequisites}
-											overall_ratings={course.overall_ratings}
-											reviews_count={course.reviews_count}
-										/>
-									);
-								})}
-							</div>
+								<Menu.Divider />
+								<Button title="Reset filter" fullWidth variant="filled">
+									<IconReload style={{ width: rem(18), height: rem(18) }} />
+									<Text className="ml-0.5">Reset filter</Text>
+								</Button>
+							</Menu.Dropdown>
+						</Menu>
 
-							<div className="mt-auto flex items-center justify-center">
-								<Pagination withEdges total={10} />
-							</div>
+						{/* Search Button */}
+						<Button type="submit">
+							<IconSearch className="size-4" aria-label="Filter" />
+						</Button>
+					</Group>
+				</form>
+
+				{/* Courses List Body */}
+				<Paper bg="dark.8" p="sm" withBorder className="h-full">
+					<div className="relative flex size-full flex-col">
+						<div className="grid grid-cols-12 grid-rows-3 gap-x-2 gap-y-2">
+							{filteredCourses.map((course) => {
+								return (
+									<CourseCard
+										// Ensure that the key is unique, otherwise same keys will cause a lot of issues.
+										key={`CourseCard_${(course.courseCode, course.courseName)}`}
+										courseName={course.courseName}
+										courseCode={course.courseCode}
+										coursePrerequisites={course.coursePrerequisites}
+										courseRating={course.courseRating}
+										courseReviewCount={course.courseReviewCount}
+									/>
+								);
+							})}
 						</div>
-					</Paper>
-				</Stack>
-			</Grid.Col>
+
+						<div className="mt-auto flex items-center justify-center">
+							<Pagination withEdges total={10} />
+						</div>
+					</div>
+				</Paper>
+			</Stack>
+			{/* </Grid.Col> */}
 
 			{/* Review Feed */}
-			{/* <Grid.Col visibleFrom="xl" span={2}>
-				<Paper bg="dark.8" p="sm" withBorder className="h-full">
-					Lol
-				</Paper>
-			</Grid.Col> */}
-		</Grid>
+			{/*<Grid.Col visibleFrom="xl" span={2}>
+          <Paper bg="dark.8" p="sm" withBorder className="h-full">
+            Lol
+          </Paper>
+        </Grid.Col>*/}
+		</Container>
 	);
 }
