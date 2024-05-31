4db97aa45f4ef9494dbce83b969e6f475d2965dc
Flora <eimayzonzon@gmail.com>
2024-05-31 20:50:14 +0700

    Guideline Page

--- apps/web/app/guideline/data.js
+++ apps/web/app/guideline/data.js
@@ -59,6 +59,25 @@ export const plus = [
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 </svg>
+ ]
 
+ export const book = [
+    <svg xmlns="http://www.w3.org/2000/svg" fill="blue" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+</svg>
+
+ ]
+ export const key = [
+    <svg xmlns="http://www.w3.org/2000/svg" fill="yellow" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="size-10">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
+</svg>
+
+ ]
+ export const list = [
+    <svg xmlns="http://www.w3.org/2000/svg" fill="yellow" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+</svg>
+
+  
 
  ]
--- apps/web/app/guideline/page.tsx
+++ apps/web/app/guideline/page.tsx
@@ -1,96 +1,94 @@
 "use client";
-import React, { useState } from 'react'
-import { guideline, aspects, plus, minus } from './data';
+import React, { useState } from "react";
+import { guideline, aspects, plus, minus, book, key, list } from "./data";
+import { Container, Text, Title } from "@mantine/core";
+
 interface Aspects {
-    title: string;
-    info : string;
+  title: string;
+  info: string;
 }
-interface Guideline{
-    title : string;
-    rule1?: string;
-    rule2?:string;
-    rule3?: string;
-    rule4?: string;
+
+interface Guideline {
+  title: string;
+  rule1?: string;
+  rule2?: string;
+  rule3?: string;
+  rule4?: string;
 }
-export default function Guideline() :JSX.Element {
-  const [openItem,setOpenItem] = useState<number[]>([]);
-  const handleclick = (index : number)=>{
-    setOpenItem((previous)=> {
-      if(previous.includes(index)){
-        return previous.filter((i)=> i!== index);
-      }else{
-        return [...previous, index]
+
+export default function Guideline(): JSX.Element {
+  const [openItem, setOpenItem] = useState<number[]>([]);
+
+  const handleclick = (index: number) => {
+    setOpenItem((previous) => {
+      if (previous.includes(index)) {
+        return previous.filter((i) => i !== index);
+      } else {
+        return [...previous, index];
       }
-    })
+    });
+  };
 
-  }
-
-  
   return (
-    <div className=' w-full  text-white h-full'>
-        <div className="max-w-4xl mx-auto px-4  ">
-        <h1 className="text-3xl font-bold text-center mb-6">Guidelines for Writing Course Reviews</h1>
-      <p className="text-gray-400 mb-8">
-        While reviews are helpful, always consult with your academic advisors when selecting courses. This platform is intended to supplement their guidance with student experiences. Remember that reading reviews is useful, but you must study according to your curriculum and requirements.
-      </p>
-
-      <div className="mb-9">
-      <h1 className="text-3xl font-bold text-center mb-8">Consider Key Aspects of the Course</h1>
-      {
-        aspects?.map((info : Aspects, index)=>(
-          <div className="mb-6" key={index}>
-          <h2 className="text-xl font-semibold mb-2">{info.title}</h2>
-          <p className="text-gray-400">{info.info}</p>
+    <Container>
+      <div className="max-w-4xl mx-auto px-4 pb-4">
+        <Title order={2} fw={700} className="flex items-center justify-center mb-6 gap-2">
+          <span className=" animate-bounce">{book}</span>
+          Guidelines for Writing Course Reviews
+          <span className=" animate-bounce">{book}</span>
+        </Title>
+        <Text c={"rgba(156, 163, 175, 1)"} className="mb-8">
+          While reviews are helpful, always consult with your academic advisors when selecting courses. This platform is
+          intended to supplement their guidance with student experiences. Remember that reading reviews is useful, but
+          you must study according to your curriculum and requirements.
+        </Text>
+        <div className="mb-9">
+          <Title order={2} fw={700} className="flex items-center justify-center mb-6 gap-2">
+            <span className=" animate-bounce">{key}</span>
+            Consider Key Aspects of the Course
+            <span className=" animate-bounce">{key}</span>
+          </Title>
+          {aspects?.map((info: Aspects, index) => (
+            <div className="mb-7" key={index}>
+              <Title order={4} fw={700} className="flex items-center gap-2 mb-2">
+                {list}
+                {info.title}
+              </Title>
+              <Text c={"rgba(156, 163, 175, 1)"}>{info.info}</Text>
+            </div>
+          ))}
         </div>
-
-        ))
-      } 
-    </div>
-
-      {
-        guideline?.map((list : Guideline, index: number)=>(
-          <div className={`mb-8 ${
-            openItem.includes(index)?"" : "bg-white text-black"
-          }`}  onClick={()=>handleclick(index)} key={index}>
-              <div className = {`flex justify-between items-center p-2 shadow-lg ${
-                openItem.includes(index)?"bg-blue-600 text-white" : ""
-              }`}>
-                <h2 className="text-lg font-semibold pl-3">{list.title}</h2>
-              {openItem.includes(index)?<span className=' cursor-pointer'>{minus}</span> :<span className=' cursor-pointer'>{plus}</span> }
-                
-              </div>
-            
-              {
-                openItem.includes(index)&&(<ul className="list-disc pl-6" >
-      
-                {list.rule1 && <li className="mb-2">{list.rule1}</li> }
+        {guideline?.map((list: Guideline, index: number) => (
+          <div
+            className={`mb-6 rounded-md ${openItem.includes(index) ? "" : "bg-white text-black"}`}
+            onClick={() => handleclick(index)}
+            key={index}
+          >
+            <div
+              className={`flex select-none cursor-pointer justify-between items-center px-3 py-3 shadow-lg ${
+                openItem.includes(index) ? "bg-blue-600 rounded-md text-white" : ""
+              }`}
+            >
+              <Title order={4} fw={600}>
+                {list.title}
+              </Title>
+              {openItem.includes(index) ? (
+                <span className=" cursor-pointer">{minus}</span>
+              ) : (
+                <span className=" cursor-pointer">{plus}</span>
+              )}
+            </div>
+            {openItem.includes(index) && (
+              <ul className="list-disc pl-6">
+                {list.rule1 && <li className="mb-2">{list.rule1}</li>}
                 {list.rule2 && <li className="mb-2">{list.rule2}</li>}
-                {list.rule3 && <li className="mb-2">{list.rule3}</li> }
-                {list.rule4 && <li className="mb-2">{list.rule4}</li> }
-
+                {list.rule3 && <li className="mb-2">{list.rule3}</li>}
+                {list.rule4 && <li className="mb-2">{list.rule4}</li>}
               </ul>
-
-                )
-      
-              }
-
-
-          
-        </div>
-
-        ))
-      }
-
-     
-
-     
-
-        </div>
-    </div>
-    
-      
-      
-    
-  )
+            )}
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
 }
-
